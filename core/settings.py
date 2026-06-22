9ed59ee0fa03cb9389d8277e45940e7eb49f8e48
naveedkhan-1996 <naveed.khan199639@gmail.com>
2026-06-22 23:20:30 +0500

    django project initial commit

--- core/settings.py
+++ core/settings.py
@@ -83,11 +83,11 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'portfolio_db',
-        'USER': 'postgres',      # Your PostgreSQL username
-        'PASSWORD': '12345',  # Your PostgreSQL password
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
